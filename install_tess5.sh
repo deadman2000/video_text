8f943b328cf9b409fe6518ad30e4ec9030bed604
Иван Марчуков <deadman2000@mail.ru>
2022-02-16 11:21:22 +0300

    install tess5 script

--- install_tess5.sh
+++ install_tess5.sh
@@ -1,4 +1,4 @@
 apt-get install software-properties-common
 add-apt-repository ppa:alex-p/tesseract-ocr5
 apt update
-apt install tesseract-ocr libicu-dev libpango1.0-dev libcairo2-dev
+apt install tesseract-ocr
